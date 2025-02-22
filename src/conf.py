583e54f0c09b460a488592ead7a48d4ad59b99b5
Scrodinger <schrodinger.grigorii@gmail.com>
2025-02-23 00:16:22 +0300

    add deploy file

--- src/conf.py
+++ src/conf.py
@@ -1,5 +1,5 @@
 from src.redis.RedisManager import RedisQueue
 from src.request.RequestHandler import RequestHandler
 
-api = RequestHandler(base_url="http://0.0.0.0:8000/")
-redis = RedisQueue(queue_name="filter", host="localhost", port=6379, db=0)
+api = RequestHandler(base_url="http://emily-database-handler-container:8000/")
+redis = RedisQueue(queue_name="filter", host="redis", port=6379, db=0)
