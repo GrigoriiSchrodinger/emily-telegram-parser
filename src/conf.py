1da621aed07b911b93ecdd55dc839ba50c7ebd8e
Scrodinger <schrodinger.grigorii@gmail.com>
2025-03-02 01:09:26 +0300

    ref conf

--- src/conf.py
+++ src/conf.py
@@ -1,5 +1,5 @@
 from src.redis.RedisManager import RedisQueue
 from src.request.RequestHandler import RequestHandler
 
-api = RequestHandler(base_url="http://emily-database-handler:8000/")
+api = RequestHandler(base_url="http://emily-database-handler:8000")
 redis = RedisQueue(queue_name="filter", host="redis", port=6379, db=0)
